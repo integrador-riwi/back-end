0e46ed6a4f4fcff34cef331ffd979f677522c658
Sebastian <zain-wave@outlook.com>
2026-02-16 20:17:24 -0500

    test get user basic testing

--- src/modules/users/users.routes.js
+++ src/modules/users/users.routes.js
@@ -1,8 +1,13 @@
-import { Router } from 'express';
+import { Router } from "express";
+import pool from "../../db/pool.js";
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Users API' });
+router.get("/", (req, res) => {
+  res.json({ message: "Users API" });
 });
 
+router.get("/test", async (req, res) => {
+  const result = await pool.query("SELECT * FROM users LIMIT 10");
+  res.json(result.rows);
+});
 export default router;
