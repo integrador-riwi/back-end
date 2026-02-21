3dbcaf8b0eb43426ab85a178a44bbc1613b9f817
Sebastian <zain-wave@outlook.com>
2026-02-20 21:03:44 -0500

    feat: add CORS configuration for frontend integration

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import authRoutes from './modules/auth/auth.routes.js';
 import usersRoutes from './modules/users/users.routes.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import config from './config/env.js';
 
 const app = express();
+
+const allowedOrigins = [
+  'https://front-end-olive-six.vercel.app',
+  'http://localhost:5173',
+  'http://localhost:3000'
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
+    }
+  },
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
